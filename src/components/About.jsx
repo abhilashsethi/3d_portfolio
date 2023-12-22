5f9c4600b707a5562408d60e088e150365c3cd09
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-12-22 20:09:55 +0530

    build issue resolved

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import Tilt from "react-tilt";
-import { motion } from "framer-motion";
-import { styles } from "../styles";
-import { services } from "../constants";
-import { fadeIn, textVariant } from "../utils";
+// import Tilt from "react-tilt";
+// import { motion } from "framer-motion";
+// import { styles } from "../styles";
+// import { services } from "../constants";
+// import { fadeIn, textVariant } from "../utils";
 
 const About = () => {
 	return (
 		<>
-			<motion.div variants={textVariant()}>
+			{/* <motion.div variants={textVariant()}>
 				<p className={styles.sectionSubText}>Introduction</p>
 				<h2 className={styles.sectionHeadText}>Overview</h2>
 			</motion.div>
-			<motion.p variants={fadeIn("", "", 0.1, 1)}></motion.p>
+			<motion.p variants={fadeIn("", "", 0.1, 1)}></motion.p> */}
 		</>
 	);
 };
